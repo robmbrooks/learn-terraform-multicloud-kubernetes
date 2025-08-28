b24af7e936b27b2f98eb6f729c4fc6084e916410
Robert Brooks <robmbrooks@gmail.com>
2025-08-27 19:15:50 -0700

    update azure version to work on current azure

--- counting-service/versions.tf
+++ counting-service/versions.tf
@@ -3,6 +3,11 @@
 
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.22.0"
